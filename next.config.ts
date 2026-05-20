245cccefd9560ba5da54c281345077351e4f0322
nepolionchakma <nepolionchakma.nc@gmail.com>
2026-05-20 11:03:07 +0600

    Image and post update - postgresql-high-availability and also reverse next config

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   // allowedDevOrigins: ["192.168.68.200:3000", "192.168.68.200"],
-  allowedDevOrigins: ["*"],
+  // allowedDevOrigins: ["*"],
 };
 
 export default nextConfig;
